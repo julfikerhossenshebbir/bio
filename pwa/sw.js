93a7954302cacd901c171a33b84e54940e6c5c71
Md. Julfiker Hossen Shebbir <julfikerhossenshebbir@gmail.com>
2024-11-28 19:26:18 +0600

    Create sw.js

---
+++ pwa/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = ["/index.html", "/styles.css", "/script.js", "/contact.html", "/about.html", "/404.html","/.htaccess"];
+
+// Install Service Worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch data from cache or network
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
