37c2f959bf01b2dcf6ec012929f3683c0c12c07a
Md. Julfiker Hossen Shebbir <julfikerhossenshebbir@gmail.com>
2024-12-09 08:26:56 +0600

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,40 +1,44 @@
-const CACHE_NAME = 'cool-cache-v2';
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to precache here:
 const PRECACHE_ASSETS = [
     './',
     './index.html',
     './styles.css',
     './script.js',
     './contact.html',
-    './about.html',
-    './offline.html'
-];
+    './about.html'
+]
 
 // Listener for the install event - precaches our assets list on service worker install.
 self.addEventListener('install', event => {
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE_NAME);
-        await cache.addAll(PRECACHE_ASSETS);
+        cache.addAll(PRECACHE_ASSETS);
     })());
 });
 
-// Activate event - clean up old caches if present
 self.addEventListener('activate', event => {
-    event.waitUntil((async () => {
-        const cacheNames = await caches.keys();
-        await Promise.all(
-            cacheNames.map(name => {
-                if (name !== CACHE_NAME) {
-                    return caches.delete(name);
-                }
-            })
-        );
-    })());
-    return self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
-// Fetch event - serve resources from cache, fallback to offline.html if offline
 self.addEventListener('fetch', event => {
-    event.respondWith((async () => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
+});    event.respondWith((async () => {
         try {
             // Try fetching from the network
             const response = await fetch(event.request);
